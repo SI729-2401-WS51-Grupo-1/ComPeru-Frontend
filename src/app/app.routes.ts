17e286b10d7885ad7cacbc03fc2bdabd3db5f7f0
ElvDev05 <vasquezelverth@gmail.com>
2024-05-14 01:37:29 -0500

    chore: first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {UserPageComponent} from "./public/pages/user-page/user-page.component";
+import {EntrepreneurPageComponent} from "./public/pages/entrepreneur-page/entrepreneur-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'users',component: UserPageComponent},
+  {path: 'entrepreneur', component: EntrepreneurPageComponent},
+  { path: '', redirectTo: 'users', pathMatch: 'full' }
+
+];
---
+++ src/app/public/pages/entrepreneur-page/entrepreneur-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrepreneur-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './entrepreneur-page.component.html',
+  styleUrl: './entrepreneur-page.component.css'
+})
+export class EntrepreneurPageComponent {
+
+}
