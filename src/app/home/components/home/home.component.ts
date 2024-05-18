47a2d85be7595f7f80c1bf68a5b72020682864f8
Marco Collantes Artola <u201410183@upc.edu.pe>
2024-05-18 13:52:40 -0500

    feat: home page v1

---
+++ src/app/home/components/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from "../../../public/components/header/header.component";
+import { FooterComponent } from "../../../public/components/footer/footer.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { NgFor } from "@angular/common";
+import {Offer} from "../../model/offer.entity";
+import {OfferService} from "../../services/offer.service";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, MatCardModule, MatButtonModule, NgFor],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+
+  offers: Array<Offer> = [];
+
+  constructor(private productsService: OfferService) {
+  }
+
+  private getAllOffers() {
+    this.productsService.getAll().subscribe((response: any) => {
+      this.offers=response
+    });
+  }
+
+  ngOnInit(): void {
+    this.getAllOffers();
+  }
+}
+
