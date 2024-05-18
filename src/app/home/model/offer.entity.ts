46349e7d57582a011d3df8632e33c480dc97c562
Marco Collantes Artola <u201410183@upc.edu.pe>
2024-05-18 13:53:30 -0500

    feat: services for home & data

---
+++ src/app/home/model/offer.entity.ts
@@ -0,0 +1,14 @@
+export class Offer {
+  id: number;
+  name: string;
+  photo: string;
+  price: number;
+  offer: number;
+  constructor() {
+    this.id = 0;
+    this.name = "";
+    this.photo = "";
+    this.price = 0.0;
+    this.offer = 0.0;
+  }
+}
---
+++ src/app/home/services/offer.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from "../../shared/services/base.service";
+import { HttpClient } from "@angular/common/http";
+import { Offer } from "../model/offer.entity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OfferService extends BaseService<Offer>{
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint='/offers'
+  }
+}
