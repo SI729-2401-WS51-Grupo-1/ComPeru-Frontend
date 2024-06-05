43992be9f7fd59ea0cdb44345277079de7c678a9
diego5m <164823084+diego5m@users.noreply.github.com>
2024-06-05 18:55:47 -0500

    feat: orders

--- src/app/admin-orders/pages/orders-management/orders-management.component.ts
+++ src/app/admin-orders/pages/orders-management/orders-management.component.ts
@@ -1,12 +1,75 @@
 import { Component } from '@angular/core';
 
+// Imports for component
+import { AfterViewInit, OnInit, ViewChild} from '@angular/core';
+
+// Material imports
+import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort, MatSortModule } from "@angular/material/sort";
+import { MatIconModule } from "@angular/material/icon";
+
+// Order imports
+import { OrdersService } from "../../services/orders.service";
+import { Order } from "../../../shared/model/order.entity";
+//import { OrderCreateAndEditComponent } from "../../components/order-create-and-edit/order-create-and-edit.component";
+import { NgClass } from "@angular/common";
+import {RouterLink} from "@angular/router";
+
 @Component({
-  selector: 'app-orders-management',
+  selector: 'app-order-management',
   standalone: true,
-  imports: [],
+  // Declare imports
+  imports: [MatPaginator, MatSort, MatSortModule, MatIconModule, MatInputModule, /*OrderCreateAndEditComponent*/ MatTableModule, NgClass, RouterLink],
   templateUrl: './orders-management.component.html',
   styleUrl: './orders-management.component.css'
 })
-export class OrdersManagementComponent {
+
+export class OrdersManagementComponent implements OnInit, AfterViewInit{
+
+  // Attributes
+  orderData: Order;
+  // Table
+  dataSource!:MatTableDataSource<any>;
+  // Table Columns
+  displayedColumns: string[] = ['id', 'userId', 'productId', 'shippingAddress', 'orderDate', 'orderStatus', 'totalPrice' ];
+  @ViewChild(MatPaginator, { static: false}) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: false}) sort!: MatSort;
+  isEditMode: boolean;
+
+  // Constructor
+  constructor(private ordersService: OrdersService){
+    this.isEditMode = false;
+    this.orderData = {} as Order;
+    this.dataSource = new MatTableDataSource<any>();
+  }
+
+  /*private resetEditState(): void{
+    this.isEditMode = false;
+    this.orderData = {} as Order;
+  }*/
+
+  // CRUD methods
+  private getAllorders() {
+    this.ordersService.getAll().subscribe((response: any) => {
+      this.dataSource.data = response;
+    });
+  };
+
+
+  // UI Event Handlers
+
+
+  // Lifecycle Hooks
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnInit(): void {
+    this.getAllorders();
+  }
+
 
 }
