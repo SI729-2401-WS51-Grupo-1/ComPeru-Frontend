e890c1026b43b8a55b0164215b09c7757adff9fe
ElvDev05 <vasquezelverth@gmail.com>
2024-06-08 22:12:32 -0500

    feat(product-detail): Aggregate comparator price

--- src/app/price-comparator/components/comparator/comparator.component.ts
+++ src/app/price-comparator/components/comparator/comparator.component.ts
@@ -3,7 +3,7 @@ import {Product} from "../../../shared/model/product.entity";
 import {ProductsService} from "../../../admin-products/services/products.service";
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {RouterLink} from "@angular/router";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {MatPaginatorModule} from "@angular/material/paginator";
@@ -20,14 +20,14 @@ import {UsersService} from "../../../admin-products/services/users.service";
   standalone: true,
   imports: [MatButton, MatInputModule,
     MatCardModule, MatPaginatorModule, MatSortModule, MatFormFieldModule,
-    MatIconModule, MatTableModule, NgClass, CurrencyPipe,MatOption, MatSelect, RouterLink],
+    MatIconModule, MatTableModule, NgClass, CurrencyPipe, MatOption, MatSelect, RouterLink, MatIconButton],
   templateUrl: './comparator.component.html',
   styleUrl: './comparator.component.css'
 })
 export class ComparatorComponent implements OnInit{
   @Input() product: Product;
   dataSource!:MatTableDataSource<any>;
-  displayedColumns: string[] = ['name', 'price', 'seller'];  // Columnas para la tabla
+  displayedColumns: string[] = ['name', 'price', 'seller','actions'];  // Columnas para la tabla
 
   constructor(private productService:ProductsService, private userService:UsersService) {
     this.product = new Product();
@@ -37,16 +37,12 @@ export class ComparatorComponent implements OnInit{
 
   private getAllProducts() {
     this.productService.getAll().subscribe((productsResponse: any) => {
-      // Filtrar productos similares por nombre o atributo
       const filteredProducts = productsResponse.filter((p: Product) => this.isSimilar(p));
 
-      // Ordenar productos por precio (ascendente)
       const sortedProducts = filteredProducts.sort((a: Product, b: Product) => a.price - b.price);
 
-      // Tomar los primeros 3 productos o menos
       const topProducts = sortedProducts.slice(0, 3);
 
-      // Obtener los nombres de los vendedores para los productos filtrados
       this.enrichWithSellerNames(topProducts);
     });
   }
@@ -61,14 +57,14 @@ export class ComparatorComponent implements OnInit{
 
     this.userService.getAll().subscribe((usersResponse: any) => {
       const enrichedProducts = products.map(product => {
-        const seller = usersResponse.find((user: any) => user.id === product.idEntrepreneur);
+        const seller = usersResponse.find((user: any) => user.id == product.idEntrepreneur);
         return {
+          id:product.id,
           name: product.name,
           price: product.price,
           sellerName: seller ? `${seller.name} ${seller.lastName}` : 'Unknown Seller'
         };
       });
-
       this.dataSource.data = enrichedProducts;
     });
   }
